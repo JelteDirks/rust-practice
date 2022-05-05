f4c25874eb2671de0f4ee4e2db98f9434109762c
Jelte Dirks <j.dirks@outlook.com>
2022-05-05 15:42:00 +0200

    simple pattern matching calculator for + - / *
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -4,11 +4,21 @@ fn main() {
     let mut arguments: Args = args();
     let _proc = arguments.next().unwrap();
     let first = arguments.next().unwrap();
-    let operator = arguments.next().unwrap();
+    let operator = arguments.next().unwrap().chars().next().unwrap();
     let second = arguments.next().unwrap();
 
     let first_operand = first.parse::<f32>().unwrap();
     let second_operand = second.parse::<f32>().unwrap();
 
-    println!("{} {} {}", first, operator, second);
+    println!("{} {} {} = {}", first, operator, second, calculate(first_operand, second_operand, operator));
+}
+
+fn calculate(first: f32, second: f32, operator: char) -> f32 {
+    match operator {
+        '+' => first + second,
+        '-' => first - second,
+        '*' => first * second,
+        '/' => first / second,
+        _ => 0.0,
+    }
 }
