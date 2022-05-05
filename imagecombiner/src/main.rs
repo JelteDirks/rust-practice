b2264ef6b8c2bb094b24908ff29d57c0daac53d6
Jelte Dirks <j.dirks@outlook.com>
2022-05-05 17:01:23 +0200

    find image and format and check compatibility
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- imagecombiner/src/main.rs
+++ imagecombiner/src/main.rs
@@ -1,8 +1,28 @@
 mod args;
 use args::Args;
+use image::{io::*, DynamicImage, ImageFormat};
 
-fn main() {
+#[derive(Debug)]
+enum ImageDataError {
+    DifferentFormat,
+}
+
+fn main() -> Result<(), ImageDataError> {
     let arguments = Args::new();
 
-    println!("{:?}", arguments);
+    let (img1, img1_format) = find_img(arguments.img1);
+    let (img2, img2_format) = find_img(arguments.img2);
+
+    if (img1_format != img2_format) {
+        return Err(ImageDataError::DifferentFormat);
+    }
+
+    return Ok(());
+}
+
+fn find_img(path: String) -> (DynamicImage, ImageFormat) {
+    let fd = Reader::open(path).unwrap();
+    let format = fd.format().unwrap();
+    let image = fd.decode().unwrap();
+    return (image, format);
 }
