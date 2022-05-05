0da15c08b0f970ed193d28e2b74720fcc52f2926
Jelte Dirks <j.dirks@outlook.com>
2022-05-05 14:26:51 +0200

    print on one line
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -2,12 +2,13 @@ use std::env::{args, Args};
 
 fn main() {
     let mut arguments: Args = args();
-    let proc = arguments.next().unwrap();
+    let _proc = arguments.next().unwrap();
     let first = arguments.next().unwrap();
     let operator = arguments.next().unwrap();
     let second = arguments.next().unwrap();
 
-    println!("{:?}", first);
-    println!("{:?}", operator);
-    println!("{:?}", second);
+    let first_operand = first.parse::<f32>().unwrap();
+    let second_operand = second.parse::<f32>().unwrap();
+
+    println!("{} {} {}", first, operator, second);
 }
